0c1bd390b2606de34180391a744f406b81eb6751
Kip Lawrence <kip.lawrence@gmail.com>
2013-03-30 16:52:15 -0600

    Ignore project files.

---
+++ dist/skin-min.js
@@ -0,0 +1,1 @@
+function a(){}a.prototype.version=function(){return"0.0.1"};window.Skin=new a;a.prototype.version=a.prototype.version;
---
+++ dist/skin.js
@@ -0,0 +1,1 @@
+function a(){}a.prototype.test=function(){console.log("test successful!")};window.Skin=a;a.prototype.test=a.prototype.test;
